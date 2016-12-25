393f0aa586ccd8e6e7d30a799b150bed03c52522
Jacob Morehouse <jacob.morehouse@gmail.com>
2016-12-24 18:20:58 -0800

    Rewrote to proper OO standards and fixed the update SQL statement.

--- tsaCurrentAirports/Program.cs
+++ tsaCurrentAirports/Program.cs
@@ -7,40 +7,100 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace tsaCurrentAirports
 {
-    /*
+
     public class Airport
     {
-        string name;
-        string shortcode;
-        string city;
-        string state;
-        string latitude;
-        string longitude;
-        string utc;
-        string dst;
-        string precheck;
+        public string name;
+        public string shortcode;
+        public string city;
+        public string state;
+        public string latitude;
+        public string longitude;
+        public int utc;
+        public int dst;
+        public int precheck;
 
-        Airport(string iname, string ishortcode, string icity, string istate,string ilatitude, string ilongitude, string iutc, string idst, string iprecheck)
+        public Airport(string name, string shortcode, string city, string state, string latitude, string longitude, int utc, int dst, int precheck)
         {
-            name = iname;
-            shortcode = ishortcode;
-            city = icity;
-            state = istate;
+            this.name = name;
+            this.shortcode = shortcode;
+            this.city = city;
+            this.state = state;
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.utc = utc;
+            this.dst = dst;
+            this.precheck = precheck;
+        }
+    }
+    
+    class databaseOps
+    {
+        private static string connectionString = ConfigurationManager.ConnectionStrings["production"].ConnectionString;
+
+        internal static bool dataAirportExists(string ishortcode)
+        {
+            SqlConnection sqlConnection1 = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = string.Format("SELECT shortcode FROM airports WHERE shortcode = '{0}'", ishortcode);
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = sqlConnection1;
+
+            Console.WriteLine(cmd.CommandText);
+
+            sqlConnection1.Open();
+            cmd.ExecuteNonQuery();
+            bool foundAirport = cmd.ExecuteReader().HasRows;
+            sqlConnection1.Close();
+            return foundAirport;
         }
 
+        internal static void insertAirport(Airport iAirport)
+        {
+            SqlConnection sqlConnection2 = new SqlConnection(connectionString);
+            SqlCommand cmd2 = new SqlCommand();
+            cmd2.CommandText = String.Format("INSERT INTO airports (name, shortcode, city, state, latitude, longitude, utc, dst, precheck) values ('{0}','{1}','{2}','{3}',{4},{5},{6},{7},{8})", iAirport.name, iAirport.shortcode, iAirport.city, iAirport.state, iAirport.latitude, iAirport.longitude, iAirport.utc, iAirport.dst, iAirport.precheck);
+            cmd2.CommandType = CommandType.Text;
+            cmd2.Connection = sqlConnection2;
+
+            Console.WriteLine(cmd2.CommandText);
+
+            sqlConnection2.Open();
+            cmd2.ExecuteNonQuery();
+            sqlConnection2.Close();
+        }
+
+        internal static void updateAirport(Airport iAirport)
+        {
+            SqlConnection sqlConnection3 = new SqlConnection(connectionString);
+            SqlCommand cmd3 = new SqlCommand();
+            cmd3.CommandText = String.Format("UPDATE airports SET name = '{0}', shortcode = '{1}', city = '{2}', state = '{3}', latitude = {4}, longitude = {5}, utc = {6}, dst = {7}, precheck = {8} where shortcode = '{1}'", iAirport.name, iAirport.shortcode, iAirport.city, iAirport.state, iAirport.latitude, iAirport.longitude, iAirport.utc, iAirport.dst, iAirport.precheck);
+            cmd3.CommandType = CommandType.Text;
+            cmd3.Connection = sqlConnection3;
+
+            Console.WriteLine(cmd3.CommandText);
+
+            sqlConnection3.Open();
+            cmd3.ExecuteNonQuery();
+            sqlConnection3.Close();
+        } 
     }
-    */
+    
+
     class Program
     {
         static void Main(string[] args)
         {
             var theUri = "https://www.tsa.gov/data/apcp.xml";
             XmlDocument xmlResult = new XmlDocument();
-            //List<Airport> airportList = new List<Airport>();
-            
+            var listAirports = new List<Airport>();
+
             try
             {
                 HttpWebRequest webReq = WebRequest.CreateHttp(theUri);
@@ -65,28 +125,43 @@ namespace tsaCurrentAirports
 
             //At this point I have XML!
 
-            //loop over that and 
+            //loop over that XML object
             XmlNodeList xmlNodes = xmlResult.SelectNodes("/airports/airport");
             foreach (XmlNode node in xmlNodes)
             {
-                string name = node["name"].InnerText;
-                string shortcode = node["shortcode"].InnerText;
-                string city = node["city"].InnerText;
-                string state = node["state"].InnerText;
-                string latitude = node["latitude"].InnerText;
-                string longitude = node["longitude"].InnerText;
-                string utc = node["utc"].InnerText;
-                string dst = node["dst"].InnerText;
-                string precheck = node["precheck"].InnerText;
-
-                //Console.WriteLine("Name: {0} , short {1} , city {2}", name, shortcode, city);
-                
-                //sql insert command will go here;
-
-
+                Airport thisAirport = new Airport(
+                    name: node["name"].InnerText.Replace("'", "''"),
+                    shortcode: node["shortcode"].InnerText,
+                    city: node["city"].InnerText,
+                    state: node["state"].InnerText,
+                    latitude: node["latitude"].InnerText,
+                    longitude: node["longitude"].InnerText,
+                    utc: (node["utc"].InnerText.ToLower() == "true") ? 1 : 0,
+                    dst: (node["dst"].InnerText.ToLower() == "true") ? 1 : 0,
+                    precheck: (node["precheck"].InnerText.ToLower() == "true") ? 1 : 0
+                );
+                listAirports.Add(thisAirport);
             }
 
-            Console.ReadLine();
+            foreach (var ap in listAirports)
+            {
+                Console.WriteLine("AP value: {0}", ap.shortcode);
+                Console.WriteLine(databaseOps.dataAirportExists(ap.shortcode));
+
+                
+                if (databaseOps.dataAirportExists(ap.shortcode) == false)
+                {
+                    databaseOps.insertAirport(ap);
+                }
+                else
+                {
+                    databaseOps.updateAirport(ap);
+                }
+                Console.WriteLine("");
+                Console.WriteLine("");
+
+                
+            }
 
 
 
